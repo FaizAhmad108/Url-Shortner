49c619ed8288fd601b7ce9ee0b5f387120cc76e9
FaizAhmad109 <faizahmad11109@gmail.com>
2026-02-10 02:00:17 +0530

    jwt mapping 10-02-2026

--- url-shortner-sb/src/main/java/com/url/shortner/service/UserDetailsImpl.java
+++ url-shortner-sb/src/main/java/com/url/shortner/service/UserDetailsImpl.java
@@ -34,7 +34,7 @@ public class UserDetailsImpl implements UserDetails {
     }
 
     public static UserDetails build(User user){
-        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole);
         return new UserDetailsImpl(
                 user.getId(),
                 user.getEmail(),
