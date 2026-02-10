7d9df939a5555115ba507a5f9f05021728ba6eb6
FaizAhmad109 <faizahmad11109@gmail.com>
2026-02-10 23:02:44 +0530

    JWT authentication filter changes

--- url-shortner-sb/src/main/java/com/url/shortner/security/jwt/JwtAuthenticationFilter.java
+++ url-shortner-sb/src/main/java/com/url/shortner/security/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,4 @@
-package com.url.shortner.security;
+package com.url.shortner.security.jwt;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
