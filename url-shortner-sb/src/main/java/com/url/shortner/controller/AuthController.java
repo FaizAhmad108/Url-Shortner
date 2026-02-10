587a6020814c9263939a4fe1b5cf0bf46dcb4a87
FaizAhmad109 <faizahmad11109@gmail.com>
2026-02-11 00:38:40 +0530

    login user

---
+++ url-shortner-sb/src/main/java/com/url/shortner/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.url.shortner.controller;
+
+import com.url.shortner.dto.LoginRequest;
+import com.url.shortner.dto.RegisterRequest;
+import com.url.shortner.models.User;
+import com.url.shortner.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/public/login")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest){
+            return ResponseEntity.ok(userService.authenticateUser(loginRequest));
+    }
+
+    @PostMapping("/public/register")
+    public ResponseEntity<?> registerUser(@RequestBody RegisterRequest registerRequest){
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(registerRequest.getPassword());
+        user.setEmail(registerRequest.getEmail());
+        user.setRole("ROLE_USER");
+        userService.registerUser(user);
+        return ResponseEntity.ok("user registered successfully");
+
+    }
+}
