b71f650f9320f2f2c6b8cfe4c3ad356facc04aeb
FaizAhmad109 <faizahmad11109@gmail.com>
2026-02-14 00:30:43 +0530

    redirect logic

--- url-shortner-sb/src/main/java/com/url/shortner/service/UrlMappingService.java
+++ url-shortner-sb/src/main/java/com/url/shortner/service/UrlMappingService.java
@@ -89,4 +89,19 @@ public class UrlMappingService {
         return clickEvents.stream().collect(Collectors.groupingBy(click -> click.getClickDate().toLocalDate() ,
                 Collectors.counting()));
     }
+
+    public UrlMapping getOriginalUrl(String shortUrl) {
+        UrlMapping urlMapping=urlMappingRepository.findByShortUrl(shortUrl);
+        if(urlMapping!=null){
+            urlMapping.setClickCount(urlMapping.getClickCount()+1);
+            urlMappingRepository.save(urlMapping);
+
+            //Record clickEvent
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRespository.save(clickEvent);
+        }
+        return urlMapping;
+    }
 }
